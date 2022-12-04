a725792e09c6bfeab071b63c9f425e5642e9fb8e
RathodDeven <devenrathodrd@gmail.com>
2022-12-04 06:46:18 +0530

    Steps Container

--- src/Components/Steps/StepsContainer.js
+++ src/Components/Steps/StepsContainer.js
@@ -4,29 +4,56 @@ const StepsContainer = ({ step }) => {
   return (
     <>
       {step !== 0 && (
-        <div className="fixed top-10 right-10 z-10">
+        <div className="fixed top-20 right-10 z-10 border p-7 rounded-[20px]">
           <div className="flex flex-col">
-            <div className="flex flex-row">
+            <div className="flex flex-row py-4 items-center">
               {/* <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24" /> */}
               {step === 0 && (
                 <img src="/loadingCircle.gif" className="w-10 h-10" />
               )}
               {step > 0 && <img src="/checkMark.png" className="w-10 h-10" />}
-              <div>Swapped Successfully on Li.FI.</div>
+              <div className="pl-2">Swapped Successfully on Li.FI.</div>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row py-4 items-center">
               {step === 1 && (
-                <img src="/loadingCircle.gif" className="w-10 h-10" />
+                <>
+                  <img src="/loadingCircle.gif" className="w-10 h-10" />
+                  <div className="pl-2">Uploading Swap Data to IPFS</div>
+                </>
               )}
-              {step > 1 && <img src="/checkMark.png" className="w-10 h-10" />}
-              <div>Swapped Successfully on Li.FI.</div>
+              {step > 1 && (
+                <>
+                  <img src="/checkMark.png" className="w-10 h-10" />
+                  <div className="pl-2">Uploaded to IPFS</div>
+                </>
+              )}
+              {step < 1 && (
+                <>
+                  <div className="pl-2">Waiting to Upload on IPFS</div>
+                </>
+              )}
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row py-4 items-center">
               {step === 2 && (
-                <img src="/loadingCircle.gif" className="w-10 h-10" />
+                <>
+                  <img src="/loadingCircle.gif" className="w-10 h-10" />
+                  <div className="pl-2">
+                    Relaying CID to The Graph via Biconomy
+                  </div>
+                </>
               )}
-              {step > 2 && <img src="/checkMark.png" className="w-10 h-10" />}
-              <div>Swapped Successfully on Li.FI.</div>
+              {step > 2 && (
+                <>
+                  <img src="/checkMark.png" className="w-10 h-10" />
+                  <div className="pl-2">Relayed Successfully</div>
+                </>
+              )}
+              {step < 2 && (
+                <>
+                  {/* <img src="/" className="w-10 h-10" /> */}
+                  <div className="pl-2">Waiting for Generating CID</div>
+                </>
+              )}
             </div>
           </div>
         </div>
