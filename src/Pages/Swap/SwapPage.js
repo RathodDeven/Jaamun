2f6fb7304b679814ae0e164c0f09ef2f91968c93
RathodDeven <devenrathodrd@gmail.com>
2022-12-04 12:16:51 +0530

    useCallback

--- src/Pages/Swap/SwapPage.js
+++ src/Pages/Swap/SwapPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Swap from "./Swap";
 import JaMoon from "../../utils/JaMoon.json";
 import { useWallet } from "@lifi/widget/providers";
@@ -6,26 +6,30 @@ import { ethers } from "ethers";
 
 const SwapPage = () => {
   const { account } = useWallet();
-  const callSwap = async (cid) => {
-    console.log("CID", cid);
-    // calling contract function here
-    const swapContract = new ethers.Contract(
-      "0x7DA6dA0B39085568aA91bdA0a8E8043E8713eBBB",
-      JaMoon,
-      account.signer
-    );
-    const res = await swapContract.swap(cid, {
-      gasLimit: 100000,
-    });
-    console.log("res", res);
-    const receipt = await res.wait();
-    if (receipt.status === 1) {
-      console.log("receipt", receipt);
-      console.log("deployed");
-    } else {
-      console.log("failed");
-    }
-  };
+  const callSwap = useCallback(
+    async (cid) => {
+      console.log("CID", cid);
+      // calling contract function here
+      console.log("account.signer", account.signer);
+      const swapContract = new ethers.Contract(
+        "0x7DA6dA0B39085568aA91bdA0a8E8043E8713eBBB",
+        JaMoon,
+        account.signer
+      );
+      const res = await swapContract.swap(cid, {
+        gasLimit: 100000,
+      });
+      console.log("res", res);
+      const receipt = await res.wait();
+      if (receipt.status === 1) {
+        console.log("receipt", receipt);
+        console.log("deployed");
+      } else {
+        console.log("failed");
+      }
+    },
+    [account.signer]
+  );
   return (
     <div className="pt-10">
       <Swap callSwap={callSwap} />
